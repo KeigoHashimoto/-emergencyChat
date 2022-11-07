eb7ae4e58221cfa3ea91c583a007b7c26a1cc4fe
KeigoHashimoto <kei.work.info@gmail.com>
2022-11-07 23:19:56 +0900

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <!-- Styles -->
     <link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 
     <title>Helme</title>
